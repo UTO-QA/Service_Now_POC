5db3910aad0012ca8a2da518ebf4e907cf33c1ed
bhaddy <baryasom@asu.edu>
2016-11-21 15:03:25 -0700

    Initial Set Up

---
+++ service-now/src/main/java/edu/asu/service_now/service_now/App.java
@@ -0,0 +1,13 @@
+package edu.asu.service_now.service_now;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
